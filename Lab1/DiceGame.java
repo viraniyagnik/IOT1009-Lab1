d1eeabf82608c3bff5ce74028ff401ca5874f6c8
Yagnik Virani <77527826+viraniyagnik@users.noreply.github.com>
2022-02-01 16:58:05 -0500

    Lab1
    done

---
+++ Lab1/DiceGame.java
@@ -0,0 +1,48 @@
+
+public class DiceGame
+{
+
+    public static void main(String[] args)
+    {
+
+        //Create dices based on require output
+        System.out.println("Creating a default d6...");
+        Die d6 = new Die();
+        System.out.println("Creating a d20...");
+        Die d20 = new Die(20);
+        System.out.println("Creating percentile die (a special d10)...");
+        Die d10 = new Die(10);
+
+        //Shows which side up before roll method in each dice
+        System.out.println("The current side up for " + d6.getname() + " is " + d6.getCurrentsideUp());
+        System.out.println("The current side up for " + d20.getname() + " is " + d20.getCurrentsideUp());
+        System.out.println("The current side up for Percentile  is " + d10.getCurrentsideUp());
+
+        //Test roll method
+        System.out.println("\nTesting the roll method\n");
+
+        //roll d6 dice and shows it's new value
+        System.out.println("Rolling the d6...");
+        d6.roll();
+        System.out.println("The new value is " + d6.getCurrentsideUp());
+
+          //roll d20 dice and shows it's new value
+        System.out.println("Rolling the d20...");
+        d20.roll();
+        System.out.println("The new value is " + d20.getCurrentsideUp());
+
+          //roll d10 dice and shows it's new value
+        System.out.println("Rolling the Percentile...");
+        d10.roll();
+        System.out.println("The new value is " + d10.getCurrentsideUp());
+
+        //set the highest dice value
+        System.out.println("\nSetting the d20 to show 20...");
+        d20.setCurrentsideUp(20);
+        System.out.println("The side up is now " + d20.getCurrentsideUp() + ". Finally.");
+
+
+    
+
+    }
+}
---
+++ Lab1/Die.java
@@ -0,0 +1,69 @@
+//Die class with constructors
+public class Die {
+
+    //variable for name, sides and sideup
+    private String name;
+    private int NumberofSides;
+    private int CurrentsideUp;
+
+    //constructor without argument, Default name: "d6"; default number of sides: 6; random side up generated from the roll method
+    public Die()
+    {
+        name = "d6";
+        NumberofSides = 6;
+        roll();
+    }
+
+    //constructor with one argument, Number of sides is the parameter. Default type/name: "d{number of sides}" random side up generated from the roll method
+    public Die(int NumberofSides)
+    {
+        this.NumberofSides = NumberofSides;
+        name = "d" + NumberofSides;
+        roll();
+    }
+
+    //constructor with two arguments, Both the number of sides and the type are parameters; random side up generated from the roll method
+    public Die(int NumberofSides, int CurrentsideUp)
+    {
+        this.NumberofSides = NumberofSides;
+        name = "d" + NumberofSides;
+        this.CurrentsideUp = CurrentsideUp;
+    }
+
+    //For random value
+    public void roll()
+    {
+        CurrentsideUp = (int)((Math.random() * NumberofSides) + 1);
+        
+    }
+
+    //To get name
+    public String getname()
+    {
+        return name;
+    }
+
+    //To get sides
+    public int getNumberofSides()
+    {
+        return NumberofSides;
+    }
+
+    //To get current sideup
+    public int getCurrentsideUp()
+    {
+        return CurrentsideUp;
+    }
+
+    // To set number of sides
+    public void setNumberofSides(int NumberofSides)
+    {
+        this.NumberofSides = NumberofSides;
+        name = "d6";
+    }
+
+    //to set current side up
+    public void setCurrentsideUp(int CurrentsideUp) {
+        this.CurrentsideUp = CurrentsideUp;
+    }
+}
